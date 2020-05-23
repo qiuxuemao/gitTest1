29538c56290b22a58c0ff729558ff52acb0de087
qiuxuemao@163.com <qiuxuemao@163.com>
2020-05-24 07:30:48 +0800

    from c 盘

--- gitTest/src/main/java/org/example/Test.java
+++ gitTest/src/main/java/org/example/Test.java
@@ -3,5 +3,6 @@ package org.example;
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is a test");
+        System.out.println("from C 盘");
     }
 }
