7c004a8a449357692469563a9b8eb9c29d478846
qiuxuemao@163.com <qiuxuemao@163.com>
2020-05-24 10:11:54 +0800

    1011

--- gitTest/src/main/java/org/example/Test.java
+++ gitTest/src/main/java/org/example/Test.java
@@ -2,9 +2,5 @@ package org.example;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("this is a test");
-        System.out.println("from C 盘");
-        System.out.println("from d pan");
-        System.out.println("from c d merge");
     }
 }
